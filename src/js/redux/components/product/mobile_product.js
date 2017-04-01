fa1f83fdd37dd5ac3e2fbca39751f289b51c2629
BerthaPeng <858005492@qq.com>
2017-04-01 20:39:31 +0800

    添加

--- src/js/redux/components/product/mobile_product.js
+++ src/js/redux/components/product/mobile_product.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MobileHeader from '../common/mobile_header';
 import { Carousel, Steps, Icon} from 'antd';
 import * as config from  'config/app.config.js';
+import 'whatwg-fetch'
 const Step = Steps.Step;
 
 export default class MobileProduct extends React.Component{
@@ -117,6 +118,34 @@ export default class MobileProduct extends React.Component{
   componentDidMount(){
     var { id } = this.props.params;
     var options = config.default.fetchOptions('POST', 'trace_product_source', {product_instance_id: id})
+    var ajaxOptions = config.default.ajaxData( 'trace_product_source', {product_instance_id: id})
+/*    try{
+      $.ajax({
+        url: config.default.trace_product,
+        type: 'POST',
+        data: JSON.stringify(ajaxOptions),
+        contentType: "application/json",
+        dataType: "json"
+      }).done( json => {
+        alert(json);
+        var {data} = json;
+        this.setState({product_trace_list: data})
+      })
+    }catch(e){
+      alert(e);
+    }*/
+
+/*    try{
+      fetch(config.default.trace_product, options)
+      .then(res => res.json())
+      .then(json => {
+        var {data} = json;
+        this.setState({product_trace_list: data})
+      })
+      .catch(ex => {console.warn("parsed err: " + ex)})
+    }catch(e){
+      alert(e);
+    }*/
     fetch(config.default.trace_product, options)
     .then(res => res.json())
     .then(json => {
--- src/js/redux/config/app.config.js
+++ src/js/redux/config/app.config.js
@@ -14,12 +14,33 @@ var fetchOptions = function(type, event , params){
         event_id: event,
         param: params
        }
-    })    
+    })
   }
 }
 
+var ajaxData = function(event , params){
+  return {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      header :{
+        token:"",
+        client_key:""
+       },
+      data :{
+        event_id: event,
+        param: params
+       }
+    })
+  }
+}
+
+
 export default {
   fetchOptions,
+  ajaxData,
   requestUrl: 'http://119.23.132.97:8001/api',
   trace_product: 'http://119.23.132.97:8001/api/trace_product',
   product_info: 'http://119.23.132.97:8001/api/product',
